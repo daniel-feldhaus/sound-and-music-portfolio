00a54201aa869700280475315f377d7c841a6d38
Danny <danny.b.feldhaus@gmail.com>
2024-10-29 13:20:10 -0700

    Initial project configuration

--- code/adaptive-tone-control/tailwind.config.ts
+++ code/adaptive-tone-control/tailwind.config.ts
@@ -1,15 +1,14 @@
-import aspectRatio from '@tailwindcss/aspect-ratio';
-import containerQueries from '@tailwindcss/container-queries';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+// tailwind.config.ts
 import type { Config } from 'tailwindcss';
 
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+const config: Config = {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
-	theme: {
-		extend: {}
-	},
-
-	plugins: [typography, forms, containerQueries, aspectRatio]
-} satisfies Config;
+export default config;
