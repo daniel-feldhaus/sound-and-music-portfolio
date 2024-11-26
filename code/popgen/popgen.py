f9c7cdf39c6794197d9af95ac6bae42a5c580ac7
Danny <danny.b.feldhaus@gmail.com>
2024-11-26 15:05:01 -0800

    Remove most global variables

--- code/popgen/popgen.py
+++ code/popgen/popgen.py
@@ -11,6 +11,7 @@ import random
 import re
 import wave
 from typing import Dict, List, Tuple
+from dataclasses import dataclass
 
 import numpy as np
 import sounddevice as sd
@@ -79,32 +80,11 @@ def parse_db(db_string: str) -> float:
         raise ValueError
     return 10 ** (db_value / 20)
 
-
-ap = argparse.ArgumentParser()
-ap.add_argument("--bpm", type=int, default=90)
-ap.add_argument("--samplerate", type=int, default=48_000)
-ap.add_argument("--root", type=parse_note, default="C[5]")
-ap.add_argument("--bass-octave", type=int, default=2)
-ap.add_argument("--balance", type=parse_linear_knob, default="5")
-ap.add_argument("--gain", type=parse_db, default="-3")
-ap.add_argument("--output")
-ap.add_argument("--test", action="store_true", help=argparse.SUPPRESS)
-args = ap.parse_args()
-
-# Tempo in beats per minute.
-bpm: int = args.bpm
-
-# Audio sample rate in samples per second.
-samplerate: int = args.samplerate
-
-# Samples per beat.
-beat_samples = int(np.round(samplerate / (bpm / 60)))
-
 # Relative notes of a major scale.
-major_scale = [0, 2, 4, 5, 7, 9, 11]
+MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
 
 # Major chord scale tones — one-based.
-major_chord = [1, 3, 5]
+MAJOR_CHORD = [1, 3, 5]
 
 
 def note_to_key_offset(note: int) -> int:
@@ -112,20 +92,15 @@ def note_to_key_offset(note: int) -> int:
     from the corresponding root MIDI key.
     """
     scale_degree: int = note % 7
-    return note // 7 * 12 + major_scale[scale_degree]
+    return note // 7 * 12 + MAJOR_SCALE[scale_degree]
 
 
 def chord_to_note_offset(posn: int) -> int:
     """Given a position within a chord, return a scale note offset — zero-based."""
     chord_posn: int = posn % 3
-    return posn // 3 * 7 + major_chord[chord_posn] - 1
+    return posn // 3 * 7 + MAJOR_CHORD[chord_posn] - 1
 
 
-# MIDI key where melody goes.
-melody_root: int = args.root
-
-# Bass MIDI key is below melody root.
-bass_root: int = melody_root - 12 * args.bass_octave
 
 # Root note offset for each chord in scale tones — one-based.
 chord_loop = [8, 5, 6, 4]
@@ -156,7 +131,7 @@ def pick_notes(chord_root: int, n: int = 4) -> List[int]:
     return notes
 
 
-def make_note(key: int, duration_beats: int = 1) -> np.ndarray:
+def make_note(key: int, beat_samples: int, samplerate: int, duration_beats: int = 1, ) -> np.ndarray:
     """Given a MIDI key number and an optional number of beats of
     note duration, return a sine wave for that note.
     """
@@ -167,13 +142,13 @@ def make_note(key: int, duration_beats: int = 1) -> np.ndarray:
     return np.sin(time_points)
 
 
-def play(sound: np.ndarray) -> None:
+def play(sound: np.ndarray, samplerate: int) -> None:
     """Play the given sound waveform using `sounddevice`."""
     sd.play(sound, samplerate=samplerate, blocking=True)
 
 
-# Unit tests, driven by hidden `--test` argument.
-if args.test:
+def test():
+    """Unit tests, driven by hidden `--test` argument."""
     note_tests: List[Tuple[int, int]] = [
         (-9, -15),
         (-8, -13),
@@ -215,19 +190,66 @@ if args.test:
             f"{chord_pos} {expected_note_offset} {computed_note_offset}"
         )
 
-    exit(0)
+@dataclass
+class Args:
+    """Command line arguments."""
+    bpm: int
+    samplerate: int
+    root: int
+    bass_octave: int
+    balance: float
+    gain: float
+    output: str
+    test: bool
+
+def parse_args() -> Args:
+    """Parse command line arguments into a typed object."""
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--bpm", type=int, default=90)
+    ap.add_argument("--samplerate", type=int, default=48_000)
+    ap.add_argument("--root", type=parse_note, default="C[5]")
+    ap.add_argument("--bass-octave", type=int, default=2)
+    ap.add_argument("--balance", type=parse_linear_knob, default="5")
+    ap.add_argument("--gain", type=parse_db, default="-3")
+    ap.add_argument("--output")
+    ap.add_argument("--test", action="store_true", help=argparse.SUPPRESS)
+    parsed_args = ap.parse_args()
+
+    args = Args(
+        bpm=parsed_args.bpm,
+        samplerate=parsed_args.samplerate,
+        root=parsed_args.root,
+        bass_octave=parsed_args.bass_octave,
+        balance=parsed_args.balance,
+        gain=parsed_args.gain,
+        output=parsed_args.output,
+        test=parsed_args.test,
+    )
+    return args
+
 
 def main():
     """Stitch together a waveform for the desired music."""
+    args = parse_args()
+    if args.test:
+        test()
+        return
+    # Samples per beat.
+    beat_samples = int(np.round(args.samplerate / (args.bpm / 60)))
+    # MIDI key where melody goes.
+    melody_root: int = args.root
+    # Bass MIDI key is below melody root.
+    bass_root: int = melody_root - 12 * args.bass_octave
+
     sound: np.ndarray = np.array([], dtype=np.float64)
     for chord_root in chord_loop:
         notes = pick_notes(chord_root - 1)
         melody = np.concatenate(
-            [make_note(note_offset + melody_root) for note_offset in notes]
+            [make_note(note_offset + melody_root, beat_samples, args.samplerate) for note_offset in notes]
         )
 
         bass_note = note_to_key_offset(chord_root - 1)
-        bass = make_note(bass_note + bass_root, duration_beats=4)
+        bass = make_note(bass_note + bass_root, beat_samples, args.samplerate, duration_beats=4)
 
         melody_gain: float = args.balance
         bass_gain = 1 - melody_gain
@@ -239,7 +261,7 @@ def main():
         output = wave.open(args.output, "wb")
         output.setnchannels(1)
         output.setsampwidth(2)
-        output.setframerate(samplerate)
+        output.setframerate(args.samplerate)
         output.setnframes(len(sound))
 
         data = args.gain * 32767 * sound.clip(-1, 1)
@@ -247,7 +269,7 @@ def main():
 
         output.close()
     else:
-        play(args.gain * sound)
+        play(args.gain * sound, args.samplerate)
 
 if __name__ == "__main__":
     main()
